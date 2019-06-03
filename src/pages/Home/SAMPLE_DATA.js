d4feed14c7e3c8a688465b7734063e8644a7fe31
rhysgoehring <rhysgoehring@gmail.com>
2019-06-03 15:05:15 -0600

    moved sample data to separate file

---
+++ src/pages/Home/SAMPLE_DATA.js
@@ -0,0 +1,123 @@
+const SAMPLE_DATA = [
+  {
+    adult: false,
+    backdrop_path: "/v4yVTbbl8dE1UP2dWu5CLyaXOku.jpg",
+    genre_ids: (5)[(12, 14, 10749, 35, 10751)],
+    id: 420817,
+    original_language: "en",
+    original_title: "Aladdin",
+    overview:
+      "A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.",
+    popularity: 499.517,
+    poster_path: "/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg",
+    release_date: "2019-05-22",
+    title: "Aladdin",
+    video: false,
+    vote_average: 7.3,
+    vote_count: 1009
+  },
+  {
+    adult: false,
+    backdrop_path: "/v4yVTbbl8dE1UP2dWu5CLyaXOku.jpg",
+    genre_ids: (5)[(12, 14, 10749, 35, 10751)],
+    id: 420817,
+    original_language: "en",
+    original_title: "Aladdin",
+    overview:
+      "A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.",
+    popularity: 499.517,
+    poster_path: "/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg",
+    release_date: "2019-05-22",
+    title: "Aladdin",
+    video: false,
+    vote_average: 7.3,
+    vote_count: 1009
+  },
+  {
+    adult: false,
+    backdrop_path: "/v4yVTbbl8dE1UP2dWu5CLyaXOku.jpg",
+    genre_ids: (5)[(12, 14, 10749, 35, 10751)],
+    id: 420817,
+    original_language: "en",
+    original_title: "Aladdin",
+    overview:
+      "A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.",
+    popularity: 499.517,
+    poster_path: "/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg",
+    release_date: "2019-05-22",
+    title: "Aladdin",
+    video: false,
+    vote_average: 7.3,
+    vote_count: 1009
+  },
+  {
+    adult: false,
+    backdrop_path: "/v4yVTbbl8dE1UP2dWu5CLyaXOku.jpg",
+    genre_ids: (5)[(12, 14, 10749, 35, 10751)],
+    id: 420817,
+    original_language: "en",
+    original_title: "Aladdin",
+    overview:
+      "A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.",
+    popularity: 499.517,
+    poster_path: "/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg",
+    release_date: "2019-05-22",
+    title: "Aladdin",
+    video: false,
+    vote_average: 7.3,
+    vote_count: 1009
+  },
+  {
+    adult: false,
+    backdrop_path: "/v4yVTbbl8dE1UP2dWu5CLyaXOku.jpg",
+    genre_ids: (5)[(12, 14, 10749, 35, 10751)],
+    id: 420817,
+    original_language: "en",
+    original_title: "Aladdin",
+    overview:
+      "A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.",
+    popularity: 499.517,
+    poster_path: "/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg",
+    release_date: "2019-05-22",
+    title: "Aladdin",
+    video: false,
+    vote_average: 7.3,
+    vote_count: 1009
+  },
+  {
+    adult: false,
+    backdrop_path: "/v4yVTbbl8dE1UP2dWu5CLyaXOku.jpg",
+    genre_ids: (5)[(12, 14, 10749, 35, 10751)],
+    id: 420817,
+    original_language: "en",
+    original_title: "Aladdin",
+    overview:
+      "A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.",
+    popularity: 499.517,
+    poster_path: "/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg",
+    release_date: "2019-05-22",
+    title: "Aladdin",
+    video: false,
+    vote_average: 7.3,
+    vote_count: 1009
+  },
+  {
+    adult: false,
+    backdrop_path: "/v4yVTbbl8dE1UP2dWu5CLyaXOku.jpg",
+    genre_ids: (5)[(12, 14, 10749, 35, 10751)],
+    id: 420817,
+    original_language: "en",
+    original_title: "Aladdin",
+    overview:
+      "A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.",
+    popularity: 499.517,
+    poster_path: "/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg",
+    release_date: "2019-05-22",
+    title: "Aladdin",
+    video: false,
+    vote_average: 7.3,
+    vote_count: 1009
+  }
+];
+
+export default SAMPLE_DATA;
